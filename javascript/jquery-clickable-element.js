0192830d8768675e57a5bdbd82c601b85e1cfb60
Mickael Desgranges <mickael.desgranges@gmail.com>
2013-04-15 14:19:28 +0300

    Update jquery-clickable-element.js

--- javascript/jquery-clickable-element.js
+++ javascript/jquery-clickable-element.js
@@ -2,11 +2,11 @@
 * Make link with block (div)
 */
 $(function(){
+	var base = $("base");
 	$('.clickIt, .clickit').each(function() {
 	    var link = $(this).attr('data-href');
 	    if( !link ) link = $(this).find('[href]').first().attr('href');
-	    if ( link ) {
-	        var base = $("base");
+	    if ( link ) {	        
 	        $(this).click(function () {
 	        	window.location.href = (base ? base.attr("href") : "")+link;
 	        });
