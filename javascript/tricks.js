a292163f3c625d3404df1a03f68c5f359a9162b0
Mickael Desgranges <mickael.desgranges@gmail.com>
2013-05-08 16:10:46 +0300

    Update tricks.js

--- javascript/tricks.js
+++ javascript/tricks.js
@@ -1,3 +1,8 @@
+/*
+* récupérer l'url du script (pour autoload)
+*
+* http://stackoverflow.com/questions/2976651/javascript-how-do-i-get-the-url-of-script-being-called
+*/
 var getScriptURL = (function() {
     var scripts = document.getElementsByTagName('script');
     var index = scripts.length - 1;
