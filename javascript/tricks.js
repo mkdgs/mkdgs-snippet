0548e35c2a17a9e7bac37f6de9b3bf865bcae897
Mickael Desgranges <mickael.desgranges@gmail.com>
2013-05-08 16:09:19 +0300

    Create tricks.js

---
+++ javascript/tricks.js
@@ -0,0 +1,6 @@
+var getScriptURL = (function() {
+    var scripts = document.getElementsByTagName('script');
+    var index = scripts.length - 1;
+    var myScript = scripts[index];
+    return function() { return myScript.src; };
+})();
