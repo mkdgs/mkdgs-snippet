6390031483561b368379b23259e60504ad3dba83
Mickael Desgranges <mickael.desgranges@gmail.com>
2013-05-09 11:06:24 +0200

    Update jquery.plugins.template.js

--- javascript/jquery.plugins.template.js
+++ javascript/jquery.plugins.template.js
@@ -8,18 +8,35 @@
 	var methods = {};   
 	var pluginName = 'myPlugin';
 	
-	// répupère l'url du script (pour autoload)
+	/* see	
+	 * http://stackoverflow.com/questions/984510/what-is-my-script-src-url/984656#984656
+	 * http://www.glennjones.net/Post/809/getAttributehrefbug.htm
+	 * 
+	 * iterate all script to find script with right filename
+	 * this work with async and defer (but your script MUST have a unique filemane)
+	 * mozilla support document.currentScript and we use it, if is set
+	 */
+	var scriptFilename = 'jquery.plugins.template.js';	
 	var scriptUrl = (function() {
-		if( s = document.currentScript ) { // support defer & async (mozilla only)
-			return s.src; 
+		if( document.currentScript ) { // support defer & async (mozilla only)
+			return document.currentScript.src; 
 		}
 		else {
-			var ls = document.getElementsByTagName('script'),s = ls[ls.length - 1];
-		    	if (s.getAttribute.length !== undefined) return s.src
-			return s.getAttribute('src', -1);
+			var i,ls=document.getElementsByTagName('script'),l=ls.length,nf,s;
+			for (i=0;i<l;i++) {
+				s=null;
+				if (ls[i].getAttribute.length !== undefined) s=ls[i].src 
+				else s = ls[i].getAttribute('src', -1); // ie odd's
+				if ( !s ) continue; // tag with no src
+				nf = s; 
+				nf = nf.split('?')[0].split('/').pop();	// get script filename		
+				if ( nf === scriptFilename ) {
+					return s;					
+				}
+			} 
 		}
 	})();
-	
+		
 	var scriptPath =  scriptUrl.substring(0, scriptUrl.lastIndexOf('/'))+"/";
 	
 	methods.init = function(params) {	    
