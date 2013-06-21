b19b6b1e97b74b3645e436e479287939c13e7b18
Mickael Desgranges <mickael.desgranges@gmail.com>
2013-06-21 11:11:35 +0200

    Update jquery.plugins.template.js

--- javascript/jquery.plugins.template.js
+++ javascript/jquery.plugins.template.js
@@ -15,25 +15,42 @@
 	 * iterate all script to find script with right filename
 	 * this work with async and defer (but your script MUST have a unique filemane)
 	 * mozilla support document.currentScript and we use it, if is set
+	 *
+	 * this will not work with local script loaded by jQuery.getScript(),
+	 * since there is no script tag added into the dom. the script is only evaluated in global space.
+	 * http://api.jquery.com/jQuery.getScript/
+	 *  
+	 * to fix this odd, you can add a reference in meta ( meta[name=srcipt][content=url] )
+	 * when you load the script
 	 */
 	var scriptFilename = 'jquery.plugins.template.js'; // don't forget to set the filename 
 	var scriptUrl = (function() {
-		if( document.currentScript ) { // support defer & async (mozilla only)
-			return document.currentScript.src; 
-		}
-		else {
-			var i,ls=document.getElementsByTagName('script'),l=ls.length,nf,s;
-			for (i=0;i<l;i++) {
-				s=null;
-				if (ls[i].getAttribute.length !== undefined) s=ls[i].src 
-				else s = ls[i].getAttribute('src', -1); // ie odd's
-				if ( !s ) continue; // tag with no src
-				nf = s; 
-				nf = nf.split('?')[0].split('/').pop();	// get script filename		
-				if ( nf === scriptFilename ) {
-					return s;					
+		if (document.currentScript) { // support defer & async (mozilla only)
+			return document.currentScript.src;
+		} else {
+			var ls,s;
+			var getSrc = function (ls, attr) {
+				var i, l = ls.length, nf, s;
+				for (i = 0; i < l; i++) {
+					s = null;
+					if (ls[i].getAttribute.length !== undefined) { 
+						s = ls[i].getAttribute(attr, -1); 					
+					}				
+					if (!s)	continue; // tag with no src
+					nf = s;
+					nf = nf.split('?')[0].split('/').pop(); // get script filename
+					if (nf === scriptFilename) {
+						return s;
+					}
 				}
-			} 
+			};			
+			ls = document.getElementsByTagName('script');
+			s = getSrc(ls, 'src');
+			if ( !s ) { // search reference of script loaded by jQuery.getScript() in meta[name=srcipt][content=url]
+				ls = document.getElementsByTagName('meta');				
+				s = getSrc(ls, 'content');
+			}			
+			if ( s ) return s;
 		}
 		return '';
 	})();
