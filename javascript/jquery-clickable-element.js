25bc489b0d62cac3d63bbddc43a793283e638590
Mickael Desgranges <mickael.desgranges@gmail.com>
2013-09-14 09:34:38 +0200

    Update jquery-clickable-element.js
    use of jQuery instead $ in global space

--- javascript/jquery-clickable-element.js
+++ javascript/jquery-clickable-element.js
@@ -1,21 +1,23 @@
 /*
 * Make link with block (div)
 */
-$(function(){
-	var base = $("base");
+jQuery(function(){
+	var $ = jQuery;
+	var base = $("base");	
 	$('.clickIt, .clickit').each(function() {
-	    var link = $(this).attr('data-href');
-	    if( !link ) link = $(this).find('[href]').first().attr('href');
-	    if ( link ) {	        
-	        $(this).click(function () {
-		        if( link.indexOf('http') === 0 ) window.location.href = link;
-		        else window.location.href = (base ? base.attr("href") : "")+link;
-	        });
-	    }
+		var link = $(this).attr('data-href');
+		if ( !link ) link = $(this).find('[href]').first().attr('href');
+		if ( link ) {	
+			$(this).click(function () {
+				if( link.indexOf('http') === 0 ) window.location.href = link;
+				else window.location.href = (base ? base.attr("href") : "")+link;
+			});
+		}
 	});
 });
 
-$(function(){
+jQuery(function(){
+	var $ = jQuery;
 	$('.clickIt').each(function() {
 	    var link = $(this).find('[href]').first().attr('href');
 	    if ( link ) {
@@ -28,6 +30,7 @@ $(function(){
 
 for i8 and older if you use <base > fix the base bug:
 var base = $("base");
-$(this).click(function () {
+jQuery(this).click(function () {
+	var $ = jQuery;
 	window.location.href = (base ? base.attr("href") : "")+link;
 });
