e8742bf4894082477688a4c223083ee0248f66d9
Mickael Desgranges <mickael.desgranges@gmail.com>
2013-12-10 14:59:18 +0100

    Update jquery-active-element-on-url.js
    décode l'url pour matcher les url avec accent 
    
    http://stackoverflow.com/questions/2742852/unicode-characters-in-urls

--- javascript/jquery-active-element-on-url.js
+++ javascript/jquery-active-element-on-url.js
@@ -2,7 +2,7 @@ $(function(){
     var url = window.location.href;
 	var baseUrl = $4p.glob('url');
 	var rgx  = new RegExp(baseUrl);
-    url = url.replace(rgx,'').replace(/\/$/,'');
+    url = decodeURI(url.replace(rgx,'').replace(/\/$/,''));
     $('[data-active-on]').each(function(){       
         var rgx = new RegExp($(this).attr('data-active-on'));
         if ( rgx.test(url) ) {
