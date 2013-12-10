a0957ce30041dffc2360689cc289ff278ddf622e
Mickael Desgranges <mickael.desgranges@gmail.com>
2013-12-10 14:59:34 +0100

    Update jquery-active-element-on-url.js

--- javascript/jquery-active-element-on-url.js
+++ javascript/jquery-active-element-on-url.js
@@ -1,7 +1,7 @@
 $(function(){	
     var url = window.location.href;
-	var baseUrl = $4p.glob('url');
-	var rgx  = new RegExp(baseUrl);
+    var baseUrl = $4p.glob('url');
+    var rgx  = new RegExp(baseUrl);
     url = decodeURI(url.replace(rgx,'').replace(/\/$/,''));
     $('[data-active-on]').each(function(){       
         var rgx = new RegExp($(this).attr('data-active-on'));
