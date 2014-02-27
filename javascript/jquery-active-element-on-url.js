b231b743ce48731dad17e26d7f0a00d100030d40
Mickael Desgranges <mickael.desgranges@gmail.com>
2014-02-27 13:19:25 +0100

    Update jquery-active-element-on-url.js
    usage

--- javascript/jquery-active-element-on-url.js
+++ javascript/jquery-active-element-on-url.js
@@ -11,3 +11,19 @@ $(function(){
         }
     });   
 });
+
+/*
+Utilisation 
+
+tout les éléménent contenant la propriété data-active-on sont testé,
+si l'url courante correspond a l'expression régulière la class active est ajoutée.
+
+<a data-active-on="regex matching current url" >
+
+<a data-active-on="/mon/url*" > tout les url qui contienne /mon/url/quelquechose
+
+<a data-active-on="/mon/url/fichier.html$" > tout les url qui se termine par /mon/url/fichier.html
+
+<a data-active-on="^/mon/url/fichier.html$" > tout les url qui commence ET se termine par /mon/url/fichier.html
+
+<a data-active-on="/foo|/bar" > tout les url qui contienne /foo ou /bar
