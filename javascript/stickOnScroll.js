4daf5c68390d4b53932eab4b3fcab0c1f63fabeb
Mickael Desgranges <mickael.desgranges@gmail.com>
2014-04-29 11:42:43 +0200

    Update stickOnScroll.js
    jsfiddle

--- javascript/stickOnScroll.js
+++ javascript/stickOnScroll.js
@@ -1,38 +1,42 @@
 /**
 *
 * stick a box on scroll
+* 
+* test it: http://jsfiddle.net/5ABxL/4/
 *
 */
 
 <div id="sticked" data-stickOnScroll="20"> sticked at 20px from top</div>
 
 <script>
- $('#sticked[data-stickOnScroll]').each(function() {
-        var fixedTop = $(this).attr('data-stickOnScroll');
-        var top = $(this).offset().top;
-        var $el = $(this);
-        var hold = {};
-        hold.position = $el.css("position");
-        hold.top = $el.css("top");
-        hold.height = $el.css("height");
-        hold.width = $el.css("width");
-        hold.margin = $el.css("margin");
-        hold.padding = $el.css("padding");
-        hold.display = 'none';
-        $el.css('z-index', 999);
-        var $mask = $('<div />').css(hold);
-        $el.after($mask);
-        $(window).scroll(function() {
-            if ($(this).scrollTop() >= (top - fixedTop)) {
-                // var pos = fixedTop - $(window).scrollTop();
-                $el.css("position", 'fixed');
-                $el.css("top", fixedTop + "px");
-                $mask.show();
-            } else {
-                $mask.hide();
-                $el.css("position", hold.position);
-                $el.css("top", hold.top);
-            }
-        });
-});
+  $(function() {
+    $('#sticked[data-stickOnScroll]').each(function() {
+           var fixedTop = $(this).attr('data-stickOnScroll');
+           var top = $(this).offset().top;
+           var $el = $(this);
+           var hold = {};
+           hold.position = $el.css("position");
+           hold.top = $el.css("top");
+           hold.height = $el.css("height");
+           hold.width = $el.css("width");
+           hold.margin = $el.css("margin");
+           hold.padding = $el.css("padding");
+           hold.display = 'none';
+           $el.css('z-index', 999);
+           var $mask = $('<div />').css(hold);
+           $el.after($mask);
+           $(window).scroll(function() {
+               if ($(this).scrollTop() >= (top - fixedTop)) {
+                   // var pos = fixedTop - $(window).scrollTop();
+                   $el.css("position", 'fixed');
+                   $el.css("top", fixedTop + "px");
+                   $mask.show();
+               } else {
+                   $mask.hide();
+                   $el.css("position", hold.position);
+                   $el.css("top", hold.top);
+               }
+           });
+   });
+ });
 <script>
