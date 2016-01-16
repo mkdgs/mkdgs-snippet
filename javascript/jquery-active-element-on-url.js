6b18a7f23c12504d504ef57e5f17fc3c81e7a4a2
Mickael Desgranges <md-github@mkdgs.fr>
2016-01-16 10:25:57 +0100

    v2

--- javascript/jquery-active-element-on-url.js
+++ javascript/jquery-active-element-on-url.js
@@ -1,3 +1,41 @@
+ /* V2 
+ * limité au éléménet a, li, span, div, button
+ *  data-active-on=:self (pour les tag a)
+ *  data-active-on=:child (cheche le tag enfant)
+ */
+ 
+ $(function () {
+        var url = window.location.href;
+        var baseUrl = $4p.glob('url');
+        var rgxbaseUrl = new RegExp(baseUrl);
+        url = decodeURI(url.replace(rgxbaseUrl, '').replace(/\/$/, ''));
+        var escapeRegExp= function (str) {
+            return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+        };
+        $('[data-active-on]', $('a, li, span, div, button')).each(function () {  
+            var link = $(this).attr('data-active-on');
+            if ( link === ':self' ) {
+                link = $(this).attr('href');
+                link = decodeURI(link.replace(rgxbaseUrl, '').replace(/\/$/, ''));
+            } 
+            else  if ( link === ':child' ) { 
+                link = $('a',this).attr('href');
+                link = decodeURI(link.replace(rgxbaseUrl, '').replace(/\/$/, ''));
+            }            
+            link = escapeRegExp(link);            
+            // les url path/?param et path?param
+            link = link.replace(new RegExp('[^/]\\?'), '/?\\?');
+            var rgx = new RegExp(link);
+            if (rgx.test(url)) {
+                $(this).addClass('active');
+                $(this).trigger('active', [$(this)]);
+            }
+        });
+    });
+
+
+
+
 $(function(){	
     var url = window.location.href;
     var baseUrl = ''; // url du site ( $4p.glob('url'); )
